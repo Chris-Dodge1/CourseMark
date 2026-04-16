23c7e871de0b6481dc498e490f498d50081da85b
Chris Dodge <cdodge3141592@gmail.com>
2026-04-16 16:38:08 -0700

    Initial Commit

---
+++ CourseFlow/CourseFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourseFlowApp.swift
+//  CourseFlow
+//
+//  Created by Chris Dodge on 4/16/26.
+//
+
+import SwiftUI
+
+@main
+struct CourseFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
